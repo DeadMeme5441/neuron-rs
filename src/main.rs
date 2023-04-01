c484b06fc662214e1904edd638bee01370dc5a9f
DeadMeme <deadunderscorememe@gmail.com>
2023-04-01 21:32:30 +0530

    Manual neuronal integration done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,177 @@
 mod neuron;
+use neuron::{Dendrite, Neuron, Synapse};
 
-fn main() {}
+use plotly::layout::{GridPattern, LayoutGrid};
+use plotly::{Layout, Plot, Scatter};
+use rand::prelude::*;
+use std::rc::Rc;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+fn main() {
+    let input_one: Vec<f64> = generate_logic_wave(8, 100);
+    let action_potential_one: Vec<f64> = action_potential_input_encoder([input_one].to_vec());
+
+    plot_graph([action_potential_one.clone()].to_vec());
+
+    let dendrite_one: Dendrite = Dendrite::new();
+    dendrite_one.unit.as_ref().borrow_mut().potential = action_potential_one;
+
+    let input_two: Vec<f64> = generate_logic_wave(8, 100);
+    let action_potential_two: Vec<f64> = action_potential_input_encoder([input_two].to_vec());
+
+    let dendrite_two: Dendrite = Dendrite::new();
+    dendrite_two.unit.as_ref().borrow_mut().potential = action_potential_two;
+
+    let mut neuron_one: Neuron = Neuron::new();
+
+    neuron_one
+        .unit
+        .as_ref()
+        .borrow_mut()
+        .inputs
+        .push(dendrite_one);
+
+    // neuron_one
+    //     .unit
+    //     .as_ref()
+    //     .borrow_mut()
+    //     .inputs
+    //     .push(dendrite_two);
+    for time in 0..100 {
+        neuron_one.compute(time);
+        // println!("{:?}", neuron_one);
+    }
+
+    neuron_one.plot();
+}
+
+fn action_potential_input_encoder(input_signals: Vec<Vec<f64>>) -> Vec<f64> {
+    let mut potential: f64 = -70.0;
+    let threshold: f64 = 0.5;
+    let mut refract: bool = false;
+    let mut activated: bool = false;
+    let mut output_signal: Vec<f64> = Vec::new();
+    let mut reft = Instant::now();
+    let mut activation = Instant::now();
+
+    for time in 0..input_signals[0].len() {
+        let input: f64 = input_signals.iter().map(|signal| signal[time]).sum();
+
+        if activated == true {
+            let new_activation = Instant::now();
+            if new_activation.duration_since(activation).as_secs_f64() < 0.03 {
+                potential += 47.5;
+            }
+            if new_activation.duration_since(activation).as_secs_f64() > 0.03
+                && new_activation.duration_since(activation).as_secs_f64() < 0.06
+            {
+                potential -= 40.0;
+            } else if new_activation.duration_since(activation).as_secs_f64() > 0.07 {
+                refract = true;
+                activated = false;
+                reft = Instant::now();
+            }
+        } else {
+            if refract == false {
+                potential += input;
+                if input >= threshold {
+                    activation = Instant::now();
+                    activated = true;
+                    potential = -55.0;
+                } else {
+                    potential -= input;
+                }
+            } else if refract == true {
+                let new_reft = Instant::now();
+                potential += 15.5;
+                if new_reft.duration_since(reft).as_secs_f64() < 0.03 {
+                    refract = false;
+                    potential = -70.0;
+                }
+            }
+        }
+        output_signal.push(potential);
+        sleep(Duration::new(0, 10000000));
+    }
+    output_signal
+}
+
+fn plot_graph(plots: Vec<Vec<f64>>) {
+    let mut plot = Plot::new();
+
+    for elem in &plots {
+        let trace = Scatter::new((0..elem.len()).collect::<Vec<usize>>(), elem.to_vec());
+        plot.add_trace(trace);
+    }
+
+    let layout = Layout::new().grid(
+        LayoutGrid::new()
+            .rows(plots.len())
+            .columns(1)
+            .pattern(GridPattern::Independent),
+    );
+    plot.set_layout(layout);
+
+    plot.write_html("out.html");
+}
+
+fn generate_random_signal(time: usize) -> Vec<f64> {
+    let mut inps: Vec<f64> = Vec::new();
+    let mut rng = rand::thread_rng();
+    for t in 0..time {
+        inps.push(0.5 * rng.gen::<f64>());
+    }
+    inps
+}
+
+fn generate_XOR_output(input_signals: Vec<Vec<f64>>) -> Vec<f64> {
+    let mut out_wave: Vec<f64> = Vec::new();
+    for time in 0..input_signals[0].len() {
+        if input_signals[0][time] == 0.32 && input_signals[1][time] == 0.32 {
+            out_wave.push(0.0);
+        } else if input_signals[0][time] == 0.32 {
+            out_wave.push(1.0);
+        } else if input_signals[1][time] == 0.32 {
+            out_wave.push(1.0);
+        } else {
+            out_wave.push(0.0);
+        }
+    }
+
+    out_wave
+}
+
+fn generate_logic_wave(interval: usize, time: usize) -> Vec<f64> {
+    let mut inps: Vec<f64> = Vec::new();
+    for t in 0..time {
+        if t.rem_euclid(interval) == 0 && t != 0 {
+            inps.push(0.6);
+        } else {
+            inps.push(0.0);
+        }
+    }
+    inps
+}
+
+fn generate_cos_wave_based_on_time(time: usize) -> Vec<f64> {
+    let mut out_wave: Vec<f64> = Vec::new();
+
+    let e = std::f64::consts::E;
+    let now = Instant::now();
+    for t in 0..time {
+        // println!("{:?}", theta_wave(t as f64 / 2.0));
+        let new_now = Instant::now();
+        out_wave.push(theta_wave(new_now.duration_since(now).as_secs_f64()));
+        sleep(Duration::new(0, 10000000));
+    }
+
+    out_wave
+}
+
+fn theta_wave(time: f64) -> f64 {
+    let constant: f64 = (2.0 * std::f64::consts::PI) / 1.0;
+    let output: f64 = 1.0 * (constant * time).cos();
+
+    output
+}
--- src/neuron.rs
+++ src/neuron.rs
@@ -1,136 +1,162 @@
-use plotly::{Plot, Scatter};
+use plotly::layout::{GridPattern, LayoutGrid, RowOrder};
+use plotly::{Layout, Plot, Scatter};
 use rand::prelude::*;
+use std::borrow;
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-struct Dendrite {
-    uid: f64,
-    potential: f64,
-    threshold: f64,
-    refract: bool,
-    activated: bool,
-    refraction_time: f64,
-    activation_time: f64,
+#[derive(Debug)]
+pub struct Dunit {
+    pub inputs: Vec<Dendrite>,
+    pub potential: Vec<f64>,
+    pub threshold: f64,
+    pub refract: bool,
+    pub activated: bool,
+    pub refraction_time: usize,
+    pub activation_time: usize,
 }
 
-fn action_potential_generation(input_signals: Vec<Vec<f64>>) -> Vec<f64> {
-    let mut potential: f64 = 0.0;
-    let threshold: f64 = 0.6;
-    let mut refract: bool = false;
-    let mut activated: bool = false;
-    let mut output_signal: Vec<f64> = Vec::new();
-    let mut reft = Instant::now();
-    let mut activation = Instant::now();
+#[derive(Debug)]
+pub struct Sunit {
+    pub input: Dendrite,
+    pub output: Dendrite,
+}
 
-    for time in 0..input_signals[0].len() {
-        let input: f64 = input_signals.iter().map(|signal| signal[time]).sum();
+#[derive(Debug)]
+pub struct Nunit {
+    pub inputs: Vec<Dendrite>,
+    pub potential: Vec<f64>,
+    pub threshold: f64,
+    pub refract: bool,
+    pub activated: bool,
+    pub refraction_time: usize,
+    pub activation_time: usize,
+}
 
-        if activated == true {
-            let new_activation = Instant::now();
-            if new_activation.duration_since(activation).as_secs_f64() < 0.03 {
-                potential += 0.4;
+#[derive(Debug)]
+pub struct Dendrite {
+    pub unit: Rc<RefCell<Dunit>>,
+}
+
+#[derive(Debug)]
+pub struct Synapse {
+    pub unit: Rc<RefCell<Sunit>>,
+}
+
+#[derive(Debug)]
+pub struct Neuron {
+    pub unit: Rc<RefCell<Nunit>>,
+}
+
+impl Dendrite {
+    pub fn new() -> Self {
+        Self {
+            unit: Rc::new(RefCell::new(Dunit {
+                inputs: Vec::new(),
+                potential: [-70.0].to_vec(),
+                threshold: -55.0,
+                refract: false,
+                activated: false,
+                refraction_time: 0,
+                activation_time: 0,
+            })),
+        }
+    }
+}
+
+impl Neuron {
+    pub fn new() -> Self {
+        Self {
+            unit: Rc::new(RefCell::new(Nunit {
+                inputs: Vec::new(),
+                potential: [-70.0].to_vec(),
+                threshold: -55.0,
+                refract: false,
+                activated: false,
+                refraction_time: 0,
+                activation_time: 0,
+            })),
+        }
+    }
+
+    pub fn compute(&mut self, time: usize) {
+        let mut neuron = self.unit.as_ref().borrow_mut();
+        let mut potential: f64 = neuron.potential[time];
+
+        let input: f64 = neuron
+            .inputs
+            .iter()
+            .map(|signal| signal.unit.as_ref().borrow_mut().potential[time])
+            .sum();
+
+        if neuron.activated == true {
+            let new_activation = time;
+            if new_activation - neuron.activation_time < 3 {
+                potential += 47.5;
             }
-            if new_activation.duration_since(activation).as_secs_f64() > 0.03
-                && new_activation.duration_since(activation).as_secs_f64() < 0.06
+            if new_activation - neuron.activation_time >= 3
+                && new_activation - neuron.activation_time < 6
             {
-                potential -= 0.4;
-            } else if new_activation.duration_since(activation).as_secs_f64() > 0.07 {
-                refract = true;
-                activated = false;
-                reft = Instant::now();
+                potential -= 40.0;
+            } else if new_activation - neuron.activation_time >= 6 {
+                neuron.refract = true;
+                neuron.activated = false;
+                neuron.refraction_time = time;
             }
         } else {
-            if refract == false {
-                potential += input;
-                if potential > threshold {
-                    activation = Instant::now();
-                    activated = true;
-                    potential = 0.2;
+            if neuron.refract == false {
+                if input > neuron.threshold {
+                    neuron.activated = true;
+                    neuron.activation_time = time;
+                    potential = -55.0;
                 } else {
-                    potential -= input;
                 }
-            } else if refract == true {
-                let new_reft = Instant::now();
-                potential += 0.05;
-                if new_reft.duration_since(reft).as_secs_f64() < 0.03 {
-                    refract = false;
-                    potential = 0.0;
+            } else if neuron.refract == true {
+                let new_refraction = time;
+                potential += 15.5;
+                if new_refraction - neuron.refraction_time < 3 {
+                    neuron.refract = false;
+                    potential = -70.0;
                 }
             }
         }
-        output_signal.push(potential);
-        sleep(Duration::new(0, 10000000));
-    }
-    output_signal
-}
-
-fn plot_graph(plots: Vec<Vec<f64>>) {
-    let mut plot = Plot::new();
-
-    for elem in plots {
-        let trace = Scatter::new((0..elem.len()).collect::<Vec<usize>>(), elem);
-        plot.add_trace(trace);
+        neuron.potential.push(potential);
     }
 
-    plot.write_html("out.html");
-}
+    pub fn plot(&mut self) {
+        let neuron = self.unit.as_ref().borrow();
 
-fn generate_random_signal(time: usize) -> Vec<f64> {
-    let mut inps: Vec<f64> = Vec::new();
-    let mut rng = rand::thread_rng();
-    for t in 0..time {
-        inps.push(0.5 * rng.gen::<f64>());
-    }
-    inps
-}
+        let mut plots = Vec::new();
+        let inputs: Vec<Vec<f64>> = neuron
+            .inputs
+            .iter()
+            .map(|x| x.unit.as_ref().borrow().potential.clone())
+            .collect();
 
-fn generate_XOR_output(input_signals: Vec<Vec<f64>>) -> Vec<f64> {
-    let mut out_wave: Vec<f64> = Vec::new();
-    for time in 0..input_signals[0].len() {
-        if input_signals[0][time] == 0.32 && input_signals[1][time] == 0.32 {
-            out_wave.push(0.0);
-        } else if input_signals[0][time] == 0.32 {
-            out_wave.push(1.0);
-        } else if input_signals[1][time] == 0.32 {
-            out_wave.push(1.0);
-        } else {
-            out_wave.push(0.0);
+        for input in inputs {
+            plots.push(input);
         }
-    }
 
-    out_wave
-}
+        plots.push(neuron.potential.clone());
 
-fn generate_logic_wave(interval: usize, time: usize) -> Vec<f64> {
-    let mut inps: Vec<f64> = Vec::new();
-    for t in 0..time {
-        if t.rem_euclid(interval) == 0 && t != 0 {
-            inps.push(0.32);
-        } else {
-            inps.push(0.0);
+        let mut plot = Plot::new();
+
+        for elem in &plots {
+            let trace = Scatter::new((0..elem.len()).collect::<Vec<usize>>(), elem.to_vec());
+            plot.add_trace(trace);
         }
+
+        let layout = Layout::new().grid(
+            LayoutGrid::new()
+                .rows(plots.len())
+                .columns(1)
+                .pattern(GridPattern::Independent)
+                .row_order(RowOrder::TopToBottom),
+        );
+        plot.set_layout(layout);
+
+        plot.write_html("out.html");
     }
-    inps
-}
-
-fn generate_cos_wave_based_on_time(time: usize) -> Vec<f64> {
-    let mut out_wave: Vec<f64> = Vec::new();
-
-    let e = std::f64::consts::E;
-    let now = Instant::now();
-    for t in 0..time {
-        // println!("{:?}", theta_wave(t as f64 / 2.0));
-        let new_now = Instant::now();
-        out_wave.push(theta_wave(new_now.duration_since(now).as_secs_f64()));
-        sleep(Duration::new(0, 10000000));
-    }
-
-    out_wave
-}
-
-fn theta_wave(time: f64) -> f64 {
-    let constant: f64 = (2.0 * std::f64::consts::PI) / 1.0;
-    let output: f64 = 1.0 * (constant * time).cos();
-
-    output
 }
